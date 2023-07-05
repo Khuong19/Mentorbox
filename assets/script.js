5c6375f2b060c2c97a68e1cb5714c4507348669d
Khuong19 <133074738+Khuong19@users.noreply.github.com>
2023-07-05 10:46:51 +0700

    ko

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,56 @@
 const imgList = document.querySelector(".topic-img-list");
+const arrowBtn = document.querySelectorAll(".topic-list i");
+const firstImgWidth = imgList.querySelector(".img-item").offsetWidth;
+const imgListChildrens =[...imgList.children];
 
-let isDragging = false;
+let isDragging = false, startX, startScrollLeft;
 
-const dragStart = () => {
+let imgPerView = Math.round(imgList.offsetWidth / firstImgWidth);
+
+imgListChildrens.slice(-imgPerView).reverse().forEach(img => {
+    imgList.insertAdjacentHTML("afterbegin", img.outerHTML);
+});
+
+imgListChildrens.slice(0, imgPerView).forEach(img => {
+    imgList.insertAdjacentHTML("beforeend", img.outerHTML);
+});
+
+arrowBtn.forEach(btn => {
+    btn.addEventListener("click", () => {
+        imgList.scrollLeft += btn.id === "left" ? - firstImgWidth : firstImgWidth;
+    })
+})
+
+const dragStart = (e) => {
     isDragging = true;
     imgList.classList.add("dragging");
+    startX = e.pageX;
+    startScrollLeft = imgList.scrollLeft;
 }
 
 const dragging = (e) => {
     if (!isDragging) return;
-    imgList.scrollRight = e.pageX;
+    imgList.scrollLeft = startScrollLeft - (e.pageX - startX);
+}
+
+const dragStop = () => {
+    isDragging = false;
+    imgList.classList.remove("dragging");
+}
+
+const infiniteScroll = () => {
+    if (imgList.scrollLeft === 0) {
+        imgList.classList.add("no-transition");
+        imgList.scrollLeft = imgList.scrollWidth - (2 * imgList.offsetWidth);
+        imgList.classList.remove("no-transition");
+    } else if(Math.ceil(imgList.scrollLeft) === imgList.scrollWidth - imgList.offsetWidth) {
+        imgList.classList.add("no-transition");
+        imgList.scrollLeft = imgList.offsetWidth;
+        imgList.classList.remove("no-transition");
+    }
 }
 
 imgList.addEventListener("mousedown", dragStart);
 imgList.addEventListener("mousemove", dragging);
+document.addEventListener("mouseup", dragStop);
+imgList.addEventListener("scroll", infiniteScroll);
